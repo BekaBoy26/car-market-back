14b4fda5eecdbd409df173d127de846126d2d5fb
BekaBoy007 <beka030333@gmail.com>
2026-08-15 18:55:28 +0600

    Prepare backend for production

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ const bootstrap = async () => {
   const server = await createApi();
   const port = Number(process.env.PORT) || 5000;
 
-  server.listen(port, () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log(`Server is on port ${port}`);
   });
 };
 
-bootstrap();;
+bootstrap();
